1cb063c841b45721cd93cd56c1e70236b9e31323
mlietti1 <mcf.lietti@outlook.com>
2023-01-10 18:39:37 +0100

    seeder

--- database/migrations/2023_01_10_154013_create_trains_table.php
+++ database/migrations/2023_01_10_154013_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('operator', 40);
+            $table->string('arriving_from', 40);
+            $table->string('destination', 40);
+            $table->time('departure');
+            $table->time('arrival');
+            $table->string('train_number');
+            $table->tinyInteger('car_number');
+            $table->boolean('on_time')->default(1);
+            $table->boolean('is_cancelled')->default(0);
             $table->timestamps();
         });
     }
---
+++ database/seeders/TrainTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class TrainTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        for($i=0; $i < 30; $i++){
+            $new_train = new Train();
+            $new_train->operator = $faker->word();
+            $new_train->arriving_from = $faker->city();
+            $new_train->destination = $faker->city();
+            $new_train->departure = $faker->time();
+            $new_train->arrival = $faker->time();
+            $new_train->train_number = $faker->bothify('??-####');
+            $new_train->car_number = $faker->randomDigitNotNull();
+            $new_train->on_time = $faker->boolean();
+            $new_train->is_cancelled = $faker->boolean();
+            $new_train->save();
+
+        }
+
+
+    }
+}
